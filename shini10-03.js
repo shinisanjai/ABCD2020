1e88ed35061820e062adf0838ab0f0a37089262e
shinisanjai <shinisanjai@yahoo.com>
2020-03-10 11:46:20 +0530

    palintdrom prog

---
+++ shini10-03.js
@@ -0,0 +1,15 @@
+var x=121;
+var y=x;
+var rev=0;
+var rem=0;
+while(x>0)
+{
+rem=x%10;
+rev = rev*10+rem;
+x=parseInt(x/10);
+}
+console.log(rev);
+if (y==rev)
+{console.log("numer is palintdrom");}
+else
+{console.log("number is not a palintdrom");}
